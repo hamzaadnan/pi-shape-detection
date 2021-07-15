0420359b0857b544e94f75d2791d3fb9f5d3cf9b
Hamza Adnan <hamzaadnan268@outlook.com>
2021-07-15 14:22:08 +0500

    Updated Code

--- main.py
+++ main.py
@@ -8,23 +8,23 @@ cap = cv2.VideoCapture(0)
 cap.set(3, frameWidth)
 cap.set(4, frameHeight)
 
-def empty(a):
+def nothing(a):
     pass
 
-cv2.namedWindow("Parameters")
-cv2.resizeWindow("Parameters", 640, 240)
-cv2.createTrackbar("Threshold 1" , "Parameters", 20, 255, empty)
-cv2.createTrackbar("Threshold 2", "Parameters", 85, 255, empty)
+cv2.namedWindow("Threshold Parameters")
+cv2.resizeWindow("Threshold Parameters", 640, 240)
+cv2.createTrackbar("Threshold 1" , "Parameters", 20, 255, nothing)
+cv2.createTrackbar("Threshold 2", "Parameters", 85, 255, nothing)
 
-def getContours(img, imgContour):
+def makeContours(img, imgContour):
     contours, hierarcy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
-    for cnt in contours:
-        area = cv2.contourArea(cnt)
+    for contour in contours:
+        area = cv2.contourArea(contour)
         if area > 5000:
-            cv2.drawContours(imgContour, cnt, -1, (255, 0, 255), 7)
+            cv2.drawContours(imgContour, contour, -1, (255, 0, 255), 7)
             peri = cv2.arcLength(cnt, True)
-            approx = cv2.approxPolyDP(cnt, 0.02*peri, True)
+            approx = cv2.approxPolyDP(contour, 0.02*peri, True)
             x, y, w, h = cv2.boundingRect(approx)
             cv2.rectangle(imgContour, (x, y), (x + w, y + h), (0, 255, 0), 5)
             cv2.putText(imgContour, "Points: " + str(len(approx)), (x + w + 20, y + 20), cv2.FONT_HERSHEY_COMPLEX, 0.7, (0, 255, 0), 2)
@@ -37,13 +37,13 @@ while True:
     imgBlur = cv2.GaussianBlur(img, (7, 7), 1)
     imgGrey = cv2.cvtColor(imgBlur,cv2.COLOR_BGR2GRAY)
 
-    threshold1 = cv2.getTrackbarPos("Threshold 1", "Parameters")
-    threshold2 = cv2.getTrackbarPos("Threshold 2", "Parameters")
+    threshold1 = cv2.getTrackbarPos("Threshold 1", "Threshold Parameters")
+    threshold2 = cv2.getTrackbarPos("Threshold 2", "Threshold Parameters")
 
     imgCanny = cv2.Canny(imgGrey, threshold1, threshold2)
     kernel = np.ones((5, 5))
     imgDil = cv2.dilate(imgCanny, kernel, iterations=1)
-    getContours(imgDil, imgContour)
+    makeContours(imgDil, imgContour)
 
     cv2.imshow("Result", imgContour)
     if cv2.waitKey(1) & 0xFF == ord("q"):
